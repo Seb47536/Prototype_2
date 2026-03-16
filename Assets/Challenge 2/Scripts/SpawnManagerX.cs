4a0a118699a2ab401615df415f3a2a5fde4da21d
Seb47536 <a47536@alunos.ipb.pt>
2026-03-16 23:36:17 +0000

    Challenge 2
    8
    Tempos random spawn bolas

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -16,7 +16,7 @@ public class SpawnManagerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", startDelay);
     }
 
     // Spawn random ball at random x position at top of play area
@@ -28,6 +28,10 @@ public class SpawnManagerX : MonoBehaviour
         int ballIndex = Random.Range(0, ballPrefabs.Length);
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+        spawnInterval = Random.Range(3.0f, 5.0f);
+        
+        Invoke("SpawnRandomBall", spawnInterval);
     }
 
 }
