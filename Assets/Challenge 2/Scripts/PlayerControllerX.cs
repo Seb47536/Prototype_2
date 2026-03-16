d0a5b2523953622aa51dd1ac1521c53cb9394d26
Seb47536 <a47536@alunos.ipb.pt>
2026-03-16 23:40:36 +0000

    Challenge 2
    9
    Delay dog spam

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float delayDog = 15.0f;
 
     // Update is called once per frame
     void Update()
     {
+        delayDog -= 0.1f;
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && delayDog <= 0.0f)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            delayDog = 15.0f;
         }
     }
 }
